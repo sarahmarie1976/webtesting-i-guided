d4750836c53801afe86bae9be41830a9861e9318
sarahmarie1976 <sholle7@gmail.com>
2020-10-07 23:41:53 -0500

    Guided Project Completed

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,41 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
-}
+function add(args) {
+  const values = Array.isArray(args) ? args : Array.from(arguments);
+  
+     return values.reduce((sum, value) => {
+      return sum + value;
+     }, 0)
+  }
+
+
+/*
+  We want this to work with any number of parameters
+
+        const values = Array.isArray(arg) ? args : Array.from(arguments);
+
+  - All the arguments/parameters that come in, we are going to assign to a variable called args
+  - We want to convert that to an array like object
+  - If it's already an array, then we don't want to do that but we want to test to see if it is
+  - If it is then args is just going to stay what it is
+  - Otherwise, we will take the arguments parameter and turn it into an array.
+
+        return values.reduce((sum, value) => {
+          return sum + value;
+     }, 0)
+        
+  - Then we will return map reduce of values and will default it to zero
+
+  - BREAKDOWN
+
+  - When we use the reduce method on it we need to convert it into an actual array. 
+  - From 0 to unspecified number of arguments we can convert that into an array so either way 
+     an array was passed in or a comma separated lust of values 
+  - The values constant is going to be an array.
+  - Then we call reduce on it and pass a it a function and for each iteration through that
+     function it will give us the SUM and the VALUE
+  - The default initial value of sum is ZERO
+
+
+*/
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,79 @@
+const { TestScheduler } = require('jest');
 const { add } = require('./calculator.js');
 
-// test away!
+describe('calculator tests', () => {
+
+
+    describe('add() tests', () => {
+        it("should return the sum of numbers passed", () => {
+            expect(add(2, 2)).toBe(4);
+            expect(add(2, 3)).toBe(5);
+        });
+
+        it('should return 0 when called with no arguments', () => {
+
+            const expected = 0; 
+            const actual = add();
+
+            expect(actual).toBe(expected);
+            
+        });
+
+        it('should return 0 when called with no parameters', () => {
+            expect(add()).toBe(0);
+        });
+
+        it('should return the value passed when only argument is passed', () => {
+            expect(add(1)).toBe(1);
+        })
+
+        it('should handle any number of arguments, separated by comma', () => {
+            expect(add(1, 2, 3)).toBe(6);
+            expect(add(1, 2, 3, 5)).toBe(11);
+
+        });
+
+        it('should handle an array of numbers', () => {
+            expect(add([1, 2, 3])).toBe(6);
+            expect(add([1, 2, 3, 5])).toBe(11);
+        })
+    })
+})
+
+
+/*
+    - You can not put more than one expectation (expect) in the same test
+    like we have above because if one fails they all fail and all of them are required to 
+    pass for the test
+
+    - Another useful function that allows you to think about a test that needs to be written
+    but maybe you are not in the position where you want to dive deep onto it but you to put 
+    a marker in, so you can come back to it in order to address it. That is the toDo() function.
+
+    - We don't implement or pass a function or any parameters with the todo(), just takes a name. If you would try to pass something else you would get an error. This is something you would come back to. But what is interesting about this, you might have several of these in your file that you thought about but you just haven't gotten back to, you can do a search and find; however, you can just run your tests and it will tell you how many you have. 
+
+    - There is a basic pattern you want to follow kind of as you think about how to set up your tests. In some cases if they are very simple you wont find this pattern to be super helpful.
+
+    - But in other cases they are complex enough logic that is helps to separate things out.
+
+        - This is called ---->   AAA ----> Arrange  -- Act/perform the tests  -- Access
+
+    - So the AAA pattern you can follow through with getting all the values getting things set up the 
+        way you need.
+
+        ex:
+
+            const expected = 0; 
+            const actual = add();
+
+    - Perfoming the function, if you are working with mocks then this arranged section would be where you would populate the mock and get them configured properly. Tests are not as simplistic as this, they can involve quite a bit of setup in order for them to work.  You want that set up to involve as little outside of your program/system as possible
+
+
+           ex:
+
+            const actual = add();  <---- performing the function
+
+    -
+
+
+*/
